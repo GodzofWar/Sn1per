29eb1c45c6440d745a36490ad5bc50c269b61912
GodzofWar <the.pentester@protonmail.com>
2023-03-14 22:30:49 -0400

    testing to see if gau install works this time around

--- install.sh
+++ install.sh
@@ -285,9 +285,7 @@ go install github.com/harleo/asnip@latest; ln -fs ~/go/bin/asnip /usr/bin/asnip
 
 # GAU INSTALLER
 echo -e "$OKBLUE[*]$RESET Installing GAU...$RESET"
-go install github.com/lc/gau/v2/cmd/gau@latest
-#mv /root/go/bin/gau /usr/local/bin/ 2> /dev/null
-#ln -fs /root/go/bin/gau /usr/bin/gau 2> /dev/null
+go install github.com/lc/gau/v2/cmd/gau@latest; ln -fs ~/go/bin/gau /usr/bin/gau
 
 # INSTALL HTTPX
 echo -e "$OKBLUE[*]$RESET Installing HTTPX...$RESET"
